8c27a592911310418edfc0753f160b4f6fe8c607
Gustavo Carreiro <gjvcarreiro@gmail.com>
2026-01-12 11:23:35 -0300

    Bump version to 0.2.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "SideKick",
     fullname = _("Sidekick - Progress Sync"),
     description = _("Gera arquivos de progresso locais para sincronização via Syncthing."),
-    version = "0.2.0"
+    version = "0.2.1"
 }
